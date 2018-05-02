603a6708ab869d37e74921e4b6e0b81ed0d22788
njuyuanrui <njuyuanrui@163.com>
2018-05-02 21:53:44 +0800

    bugfix

--- provider/main.cpp
+++ provider/main.cpp
@@ -1,14 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "provider.h"
 #include "string.h"
 
 int main()
 {
 	string enclave_signed_so = "../isv_enclave.signed.so";
-
+	
+	//构造用于测试的需求方公钥，和enclave那边构造的相同
 	uint8_t test_req_pubkey[775]; 
 
 	memset(test_req_pubkey, 1, 775);
-
+	
+	//由于enclave那边的测试吐出的相关数据都用写文件的方式存下来，这里再读回来
 	uint8_t quote[1200];
 	uint8_t pubkey[775];
 	uint8_t sig[512];
